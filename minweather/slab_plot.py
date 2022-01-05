10000248d5ddf9ac420b90a887d649b140d16c65
Youngsung Kim <kimy@ornl.gov>
2022-01-05 15:45:09 -0500

    updated slab plot

--- minweather/slab_plot.py
+++ minweather/slab_plot.py
@@ -1,19 +1,28 @@
-import os, sys
+"""a simple slab-data plot
 
+Usage: python slab_plot.py <slab-file>
+"""
+
+import os, sys
 import matplotlib.pyplot as plt
 import pyslabs
-import numpy as np
 
 variable_names = ("dens", "umom", "wmom", "rhot")
 
 def main():
 
+    # open-slab file
     with pyslabs.open(sys.argv[1]) as data:
 
+        # iterate every variables
         for name in variable_names:
-            for time, variable in enumerate(data.get_array(name)):
+
+            # read variables from miniweather
+            for index, variable in enumerate(data.get_array(name)):
+
+                # generate contour plot
                 plt.contourf(variable)
-                plt.savefig("%s_%d.png" % (name, time))
+                plt.savefig("%s_%d.png" % (name, index))
 
 
 if __name__ == "__main__":
